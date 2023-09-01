b69948e83428879df9a23853b4fae8714fbc95ce
Trần Tiến Đạt <76415497+datbe202iters@users.noreply.github.com>
2023-09-01 11:09:37 +0700

    done structure

--- js/home.js
+++ js/home.js
@@ -7,6 +7,8 @@ document
       "0px"
     );
   });
+
+// slider
 $(".slide-nav").on("click", function (e) {
   e.preventDefault();
   // get current slide
@@ -33,4 +35,65 @@ $(".slide-nav").on("click", function (e) {
         .removeClass("animate--end flex--active");
     }, 800);
   }
+});
+
+// shop by category
+
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 5,
+  sliderPerGroup: 5,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination-shop",
+    clickable: true,
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-btn-next",
+    prevEl: ".swiper-btn-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },
+});
+var swiper = new Swiper(".slide-mini-product", {
+  slidesPerView: 2,
+  sliderPerGroup: 3,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination-mini-slide",
+    clickable: true,
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-btn-next",
+    prevEl: ".swiper-btn-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+  },
 });
